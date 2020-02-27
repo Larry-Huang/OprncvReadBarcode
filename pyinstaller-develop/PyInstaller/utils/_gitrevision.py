43b39414c875a0ffa51db16135e650eb19c92878
Jeffy <JerryHuang@sinbon.com>
2020-02-27 16:01:57 +0800

    first commit

---
+++ pyinstaller-develop/PyInstaller/utils/_gitrevision.py
@@ -0,0 +1,14 @@
+#
+# The content of this file will be filled in with meaningful data
+# when creating an archive using `git archive` or by downloading an
+# archive from github, e.g. from github.com/.../archive/develop.zip
+#
+rev = "a1f92c6a08"     # abbreviated commit hash
+commit = "a1f92c6a08b45ed53be369298c648627ba189205"  # commit hash
+date = "2020-02-10 12:19:57 -0600"   # commit date
+author = "David Maiden Mueller <dtmaidenmueller@mmm.com>"
+ref_names = "HEAD -> develop"  # incl. current branch
+commit_message = """depend: util: Ensuring repeatable builds of base_library.zip by iterating over graph nodes sorted by identifier, rather than iterating over graph nodes in arbitrary order.
+
+see https://github.com/pyinstaller/pyinstaller/issues/4654
+"""
